84c4056e85bb336cbbdda24a9a3184e6266ee493
Andrius Velykis <andrius.velykis@newcastle.ac.uk>
2008-01-01 18:48:18 +0000

    Rough scripts to sync Isabelle-hg repository to Eclipse isabelle-scala projects

---
+++ sync-isa-commits.sh
@@ -0,0 +1,131 @@
+#!/bin/sh
+#
+# Usage: ./sync-isa-commits.sh ISABELLE_REPO_DIR [FROM_REV] [TO_REV]
+#
+# Arguments:
+#   ISABELLE_REPO_DIR  the directory of Isabelle Mercurial repository
+#   FROM_REV           revision to start from (current one by default)
+#   TO_REV             revision to end sync (tip by default)
+#
+# Synchronizes commits about Isabelle Scala from Isabelle Mercurial
+# repository to isabelle-scala GIT repository. Uses the sync-isa-file.sh
+# script to sync required files
+#
+# Author: Andrius Velykis
+#
+
+ISABELLE_REPO="$1"
+
+[ -z "$ISABELLE_REPO" ] && echo "Missing Isabelle repository directory" && exit 2
+
+ISABELLE_SCALA_DIR=`pwd`
+
+AUTHOR_EXPECTED="wenzelm"
+AUTHOR_NAME="Makarius Wenzel"
+# e-mail in reverse to avoid spambots crawling the repository
+AUTHOR_EMAIL_REV="ten.siteks@suirakam"
+
+FILES_MATCH='lib/jedit/plugin\|src/Pure[^ ]*.scala\|build-jars'
+
+# ensure the repo is updated to the master
+git checkout master
+
+# Go to Isabelle folder
+cd $ISABELLE_REPO
+
+FROM_REV="$2"
+TO_REV="$3"
+
+# get revision of the current copy if unset
+[ -z "$FROM_REV" ] && FROM_REV=`hg id --num`
+
+# get revision of the tip
+[ -z "$TO_REV" ] && TO_REV=`hg tip --template '{rev}'`
+
+[ -z "$FROM_REV" ] && echo "Missing from revision" && exit 2
+[ -z "$TO_REV" ] && echo "Missing to revision" && exit 2
+
+# start from the next revision
+r=$(($FROM_REV + 1))
+
+echo "Starting sync at revision $r."
+
+while [ $r -le "$TO_REV" ]
+do
+
+  echo "Analysing revision $r..."
+
+  cd $ISABELLE_REPO
+
+  MESSAGE=`hg log --rev $r --template '{desc}'`
+  DATE=`hg log --rev $r --template '{date|rfc822date}'`
+  AUTHOR=`hg log --rev $r --template '{author}'`
+  FILES=`hg log --rev $r --template '{files}'`
+  HASH=`hg log --rev $r --template '{node}'`
+  
+#  echo "$MESSAGE"
+#  echo "$DATE"
+#  echo "$AUTHOR"
+#  echo "$FILES"
+
+  MATCH=`echo "$FILES" | grep "$FILES_MATCH"`
+#  echo "$MATCH"
+  if [ -n "$MATCH" ];
+  then
+  
+    [ "$AUTHOR" != "$AUTHOR_EXPECTED" ] && echo "Unexpected author found: $AUTHOR. Expected: $AUTHOR_EXPECTED" && exit 2;
+    
+    echo "Picking revision $r by $AUTHOR."
+    
+    NEWMESSAGE=`echo "$MESSAGE\n\nIsabelle-hg: http://isabelle.in.tum.de/repos/isabelle@$r $HASH"`
+#    echo "$NEWMESSAGE"
+
+    # update to the revision
+    hg update --clean --rev "$r"
+    
+    cd $ISABELLE_SCALA_DIR
+    
+    # launch file copy script
+    ./sync-isa-files.sh "$ISABELLE_REPO"
+    RETVAL=$?
+    [ $RETVAL -ne 0 ] && echo "Copying failed" && exit 2;
+    
+    # add-remove items
+    git add --all
+    
+    AUTHOR_EMAIL=`echo "$AUTHOR_EMAIL_REV" | rev`;
+    
+    git commit --message="$NEWMESSAGE" --date="$DATE" --author="$AUTHOR_NAME <$AUTHOR_EMAIL>";
+    
+    # Try to retain the author's date as committer date
+    LAST_COMMIT_DATE=`git log -1 --format="%ct" HEAD^1`;
+    NEW_AUTHOR_DATE=`git log -1 --format="%at" HEAD`;
+    
+    # To preserve monotonicity of commit dates, if author is before the last committer,
+    # then just increase the committer date by a short period of time
+    if [ "$NEW_AUTHOR_DATE" -gt "$LAST_COMMIT_DATE" ];
+    then
+      NEW_COMMIT_DATE="$DATE"
+    else
+      NEW_COMMIT_DATE=`expr $LAST_COMMIT_DATE + 10`
+    fi
+    
+    export NEW_COMMIT_DATE
+    
+    # rewrite the committer date of last commit
+    git filter-branch -f --env-filter \
+    'export GIT_COMMITTER_DATE="$NEW_COMMIT_DATE"' HEAD^1..HEAD
+    
+    
+#  else
+#    echo "Skipping $AUTHOR";
+  fi
+  
+  r=$(( $r + 1 ))
+done
+
+# update to the final revision (so remove 1)
+cd $ISABELLE_REPO
+r=$(( $r - 1 ))
+hg update --clean --rev "$r"
+
---
+++ sync-isa-files.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+#
+# Usage: ./sync-isa-files.sh ISABELLE_REPO_DIR
+#
+# Arguments:
+#   ISABELLE_REPO_DIR the directory of Isabelle repository
+#
+# Synchronizes Scala files from Isabelle repository to isabelle-scala
+# projects, which would be used from within Eclipse Scala-IDE.
+#
+# Author: Andrius Velykis
+#
+
+ISABELLE_REPO="$1"
+
+[ -z "$ISABELLE_REPO" ] && echo "Missing Isabelle repository directory" && exit 2
+
+ISABELLE_SCALA_DIR=`pwd`
+
+ISABELLE_JEDIT_DIR=$ISABELLE_SCALA_DIR/isabelle.jedit
+ISABELLE_JEDIT_SRC=$ISABELLE_JEDIT_DIR/src/isabelle
+
+ISABELLE_PURE_DIR=$ISABELLE_SCALA_DIR/isabelle.pure
+ISABELLE_PURE_SRC=$ISABELLE_PURE_DIR/src/isabelle
+
+ISABELLE_REPO_JEDIT_DIR=$ISABELLE_REPO/lib/jedit/plugin
+ISABELLE_REPO_PURE_DIR=$ISABELLE_REPO/src/Pure
+
+# Go to Isabelle-jEdit folder
+[ ! -d "$ISABELLE_REPO_JEDIT_DIR" ] && echo "Missing Isabelle repo jEdit folder: $ISABELLE_REPO_JEDIT_DIR" && exit 2
+cd $ISABELLE_REPO_JEDIT_DIR
+
+# Clean jEdit source folder
+rm -rf $ISABELLE_JEDIT_SRC && mkdir $ISABELLE_JEDIT_SRC
+
+# Copy jEdit source contents
+cp -rf isabelle/ $ISABELLE_JEDIT_SRC/
+
+# Copy the rest of jEdit folder
+rsync --recursive --relative --times --perms --exclude "isabelle" --exclude "src" --exclude ".classpath" --exclude ".project" --exclude ".settings" . $ISABELLE_JEDIT_DIR
+
+# Go to Isabelle-Pure folder
+[ ! -d "$ISABELLE_REPO_PURE_DIR" ] && echo "Missing Isabelle repo Pure folder: $ISABELLE_REPO_PURE_DIR" && exit 2
+cd $ISABELLE_REPO_PURE_DIR
+
+# Clean Pure source folder
+rm -rf $ISABELLE_PURE_SRC && mkdir $ISABELLE_PURE_SRC
+
+# Copy all *.scala files to Pure project
+find . -type f -iname '*.scala' -exec cp -rf \{\} "$ISABELLE_PURE_SRC" \;
